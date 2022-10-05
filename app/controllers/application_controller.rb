49ed8dd9a50268f97f5d80f234ff563ac9c0ee56
petersjang1993 <petersjang1993@gmail.com>
2022-10-05 16:47:17 +0000

    rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   layout(false)
-
+    
   # Add your actions below this line
-  # ================================
-
+  
+  def play_rock
+    render({ :template => "game_templates/rock.html.erb" })
+  end
+  
 end
